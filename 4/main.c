7aa2ab7275290f4663ddad667707327f5100bc91
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-10-05 17:01:18 +0800

    feat: challenge 4

---
+++ 4/main.c
@@ -0,0 +1,25 @@
+/*
+ * A kernel module that prints to the debug log level when loaded.
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int init_mod(void)
+{
+	printk(KERN_DEBUG "Good morning ve482!\n");
+	return 0;
+}
+
+static void cleanup_mod(void)
+{
+	printk(KERN_DEBUG "exit by ve482.\n");
+}
+
+module_init(init_mod);
+module_exit(cleanup_mod);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Frank Shi");
+MODULE_DESCRIPTION("Simple module that logs a debug message when loaded.");
